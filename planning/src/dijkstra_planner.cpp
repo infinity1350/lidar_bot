2f02c6eb270e16498de7a40f3b88ca1b2f3f67a3
infinity1350 <daisonedwin@gmail.com>
2025-12-20 13:32:23 +0530

    Fix DijkstraPlanner initialization and enhance poseCallback error handling

--- planning/src/dijkstra_planner.cpp
+++ planning/src/dijkstra_planner.cpp
@@ -21,7 +21,7 @@ namespace planning
             map_pub_ = this->createPublisher<nav_msgs::msg::OccupancyGrid>(
                 "/dijkstra/visited_map", 10);
 
-            tf_buffer_ = std::make_unique<tf2_ros::Buffer>(get_clock());
+            tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
             tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
         }
@@ -31,6 +31,42 @@ namespace planning
             map_ = map;
             visited_map_.header.frame_id = map_->header.frame_id;
             visited_map_.info = map_->info;
-            visited_map_.data = ;
+            visited_map_.data = std::vector<int8_t>(visited_map_.info.width * visited_map_.info.height, -1);
         }
+
+        void DijkstraPlanner::poseCallback(const geometry_msgs::msg::PoseStamped::SharedPtr pose){
+            
+            if(!map){
+                RCLCPP_ERROR(get_logger(), "No map received");
+                return;
+            }
+            visited_map_.data = std::vector<int8_t>(visited_map_.info.width * visited_map_.info.height, -1) 
+            geometery_msgs::msg::TransformStamped map_to_base_tf;
+            
+            try
+            {
+                map_to_base_tf = tf_buffer_.->lookupTransform(
+                    map_->header.frame_id, "base_footprint", tf2::TimePointZero);
+            }
+            catch(const tf2::TransformException &ex)
+            {
+                RCLCPP_ERROR(get_logger(), "Transform error: %s", ex.what());
+                return;
+            }
+
+            geometry_msgs::msg::Pose map_to_base_pose;
+            map_to_base_pose.position.x = map_to_base_tf.transform.translation.x;
+            map_to_base_pose.position.y = map_to_base_tf.transform.translation.y;
+            map_to_base_pose.orientation = map_to_base_tf.transform.rotation;
+
+            auto path = plan(map_to_base_pose, pose->pose);
+            if(!path.poses.empty())
+            {
+                RCLCPP_ERROR(get_logger(), "shortest path to the goal is found");
+                path->publish(path)
+            }
+        }
+
+
+
 }
