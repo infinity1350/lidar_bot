8cd6996e2e0d7b06b2617c2756c10128fcaa27a5
infinity1350 <daisonedwin@gmail.com>
2026-01-07 00:02:01 +0530

    Refactor PDMotionPlanner: add missing includes, fix typos, and declare private members

--- motion_planning/include/motion_planning/pd_motion_planner.hpp
+++ motion_planning/include/motion_planning/pd_motion_planner.hpp
@@ -1,4 +1,9 @@
 #include <rclcpp/rclcpp.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <tf_ros/buffer.h>
+#include <tf2_ros/tranform_listener.h>
 
 namespace motion_planning
 {
@@ -6,7 +11,15 @@ namespace motion_planning
     {
         public:
             PDMotionPlanner();
+        
+        private:
+            rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub_;
+            rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub_;
+            rclcpp:Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr next_pose_pub_;
 
-            path_sub_ = this->create_sub
-    }
+            std::shared_ptr<tf2_ros::TransformListener> tranform_listener_;
+            std::shared_ptr<tf2_ros:Buffer> tf_buffer_;
+            rclcpp::Timerbase::SharedPtr control_loop_;
+            
+    };
 }
